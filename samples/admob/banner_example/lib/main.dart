57d370649838816354be07148058d95f532aabf3
Reid Baker <reidbaker@google.com>
2023-02-15 15:39:16 -0500

    Update the banner example to have multiple banners in a scrolling list.

--- samples/admob/banner_example/lib/main.dart
+++ samples/admob/banner_example/lib/main.dart
@@ -20,7 +20,8 @@ class BannerExample extends StatefulWidget {
 }
 
 class BannerExampleState extends State<BannerExample> {
-  BannerAd? _bannerAd;
+  List<BannerAd?> _bannerAds = List.empty(growable: true);
+  final bannerSize = 10;
 
   final String _adUnitId = Platform.isAndroid
       ? 'ca-app-pub-3940256099942544/6300978111'
@@ -35,61 +36,168 @@ class BannerExampleState extends State<BannerExample> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Banner Example',
-      home: Scaffold(
+        title: 'Banner Example',
+        home: Scaffold(
           appBar: AppBar(
             title: const Text('Banner Example'),
           ),
-          body: Stack(
+          body: ListView(
             children: [
-              if (_bannerAd != null)
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: SafeArea(
-                    child: SizedBox(
-                      width: _bannerAd!.size.width.toDouble(),
-                      height: _bannerAd!.size.height.toDouble(),
-                      child: AdWidget(ad: _bannerAd!),
-                    ),
-                  ),
-                )
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              if (_bannerAds[0] != null)
+                SizedBox(
+                  width: _bannerAds[0]!.size.width.toDouble(),
+                  height: _bannerAds[0]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[0]!),
+                ),
+              if (_bannerAds[1] != null)
+                SizedBox(
+                  width: _bannerAds[1]!.size.width.toDouble(),
+                  height: _bannerAds[1]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[1]!),
+                ),
+              if (_bannerAds[2] != null)
+                SizedBox(
+                  width: _bannerAds[2]!.size.width.toDouble(),
+                  height: _bannerAds[2]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[2]!),
+                ),
+              if (_bannerAds[3] != null)
+                SizedBox(
+                  width: _bannerAds[3]!.size.width.toDouble(),
+                  height: _bannerAds[3]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[3]!),
+                ),
+              if (_bannerAds[4] != null)
+                SizedBox(
+                  width: _bannerAds[4]!.size.width.toDouble(),
+                  height: _bannerAds[4]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[4]!),
+                ),
+              if (_bannerAds[5] != null)
+                SizedBox(
+                  width: _bannerAds[5]!.size.width.toDouble(),
+                  height: _bannerAds[5]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[5]!),
+                ),
+              if (_bannerAds[6] != null)
+                SizedBox(
+                  width: _bannerAds[6]!.size.width.toDouble(),
+                  height: _bannerAds[6]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[6]!),
+                ),
+              if (_bannerAds[7] != null)
+                SizedBox(
+                  width: _bannerAds[7]!.size.width.toDouble(),
+                  height: _bannerAds[7]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[7]!),
+                ),
+              if (_bannerAds[8] != null)
+                SizedBox(
+                  width: _bannerAds[8]!.size.width.toDouble(),
+                  height: _bannerAds[8]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[8]!),
+                ),
+              if (_bannerAds[9] != null)
+                SizedBox(
+                  width: _bannerAds[9]!.size.width.toDouble(),
+                  height: _bannerAds[9]!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAds[9]!),
+                ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
+              const ListTile(
+                title: Text('Some title'),
+              ),
             ],
-          )),
-    );
+          ),
+        ));
   }
 
   /// Loads and shows a banner ad.
   ///
   /// Dimensions of the ad are determined by the AdSize class.
   void _loadAd() async {
-    BannerAd(
-      adUnitId: _adUnitId,
-      request: const AdRequest(),
-      size: AdSize.banner,
-      listener: BannerAdListener(
-        // Called when an ad is successfully received.
-        onAdLoaded: (ad) {
-          setState(() {
-            _bannerAd = ad as BannerAd;
-          });
-        },
-        // Called when an ad request failed.
-        onAdFailedToLoad: (ad, err) {
-          ad.dispose();
-        },
-        // Called when an ad opens an overlay that covers the screen.
-        onAdOpened: (Ad ad) {},
-        // Called when an ad removes an overlay that covers the screen.
-        onAdClosed: (Ad ad) {},
-        // Called when an impression occurs on the ad.
-        onAdImpression: (Ad ad) {},
-      ),
-    ).load();
+    for (var i = 0; i < bannerSize; i++) {
+      BannerAd(
+        adUnitId: _adUnitId,
+        request: const AdRequest(),
+        size: AdSize.banner,
+        listener: BannerAdListener(
+          // Called when an ad is successfully received.
+          onAdLoaded: (ad) {
+            setState(() {
+              _bannerAds.add(ad as BannerAd);
+            });
+          },
+          // Called when an ad request failed.
+          onAdFailedToLoad: (ad, err) {
+            ad.dispose();
+          },
+          // Called when an ad opens an overlay that covers the screen.
+          onAdOpened: (Ad ad) {},
+          // Called when an ad removes an overlay that covers the screen.
+          onAdClosed: (Ad ad) {},
+          // Called when an impression occurs on the ad.
+          onAdImpression: (Ad ad) {},
+        ),
+      ).load();
+    }
   }
 
   @override
   void dispose() {
-    _bannerAd?.dispose();
+    for (var i = 0; i < _bannerAds.length; i++) {
+      _bannerAds[i]?.dispose();
+    }
     super.dispose();
   }
 }
