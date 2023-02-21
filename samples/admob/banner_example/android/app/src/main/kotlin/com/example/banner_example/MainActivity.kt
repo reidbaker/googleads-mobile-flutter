37a2116de4f7b305410a9944264576aa886d21c1
Reid Baker <reidbaker@google.com>
2023-02-21 17:47:39 -0500

    Call super in configureFlutterEngine

--- samples/admob/banner_example/android/app/src/main/kotlin/com/example/banner_example/MainActivity.kt
+++ samples/admob/banner_example/android/app/src/main/kotlin/com/example/banner_example/MainActivity.kt
@@ -57,12 +57,13 @@ class MainActivity: FlutterActivity() {
 
     // }
 
-    // override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-    //     flutterEngine
-    //             .platformViewsController
-    //             .registry
-    //             .registerViewFactory("<platform-view-type>", NativeViewFactory())
-    // }
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine
+                .platformViewsController
+                .registry
+                .registerViewFactory("<platform-view-type>", NativeViewFactory())
+    }
 
     private val editor: SharedPreferences.Editor
     get() {
